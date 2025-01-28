ad58911424b36f060a39b1fe8e0f0d9a39f530fa
pashaimran <pashaimran68413@gmail.com>
2025-01-28 13:07:18 +0530

    version

--- provider.tf
+++ provider.tf
@@ -3,6 +3,23 @@ provider "aws" {
   region = var.aws_region  # Replace with your AWS regions
 }
 
+terraform {
+  cloud {
+    organization = "aws-infra-practice" # Replace with your Terraform Cloud organization
+
+    workspaces {
+      name = "vpc-creation" # Replace with your workspace name in Terraform Cloud
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.63.1" # Keep your existing AWS provider version
+    }
+  }
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)  # Updated
