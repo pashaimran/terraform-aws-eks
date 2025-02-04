cc2c4941ba549ab70846d82228cba8416b6dbc90
pashaimran <pashaimran68413@gmail.com>
2025-02-04 15:18:29 +0530

    version

--- varibales.tf
+++ varibales.tf
@@ -112,10 +112,10 @@ variable "node_group_tags" {
   default     = {}
 }
 
-variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instances in the EKS node group."
-  type        = string
-}
+# variable "ami_id" {
+#   description = "The AMI ID to use for the EC2 instances in the EKS node group."
+#   type        = string
+# }
 
 variable "enable_vpc_cni" {
   description = "Enable VPC CNI addon"
