18820d2f1d485f7655e61c86ab3b118958f38022
pashaimran <pashaimran68413@gmail.com>
2025-02-05 17:48:00 +0530

    version

--- iam-role-master.tf
+++ iam-role-master.tf
@@ -1,32 +1,39 @@
-resource "aws_iam_role" "eks" {
-  name = "eks-cluster-role"
+# resource "aws_iam_role" "eks" {
+#   name = "eks-cluster-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "eks.amazonaws.com"
+#       }
+#       Action = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
+# resource "aws_iam_role_policy_attachment" "eks_policy" {
+#   role       = aws_iam_role.eks.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+# }
 
-resource "aws_eks_identity_provider_config" "oidc" {
-  cluster_name = aws_eks_cluster.main.name
+# resource "aws_eks_identity_provider_config" "oidc" {
+#   cluster_name = aws_eks_cluster.main.name
 
-  oidc {
-    client_id                     = "sts.amazonaws.com"
-    identity_provider_config_name = "${aws_eks_cluster.main.name}-oidc"
-    issuer_url                    = aws_eks_cluster.main.identity.0.oidc.0.issuer
-  }
-}
+#   oidc {
+#     client_id                     = "sts.amazonaws.com"
+#     identity_provider_config_name = "${aws_eks_cluster.main.name}-oidc"
+#     issuer_url                    = aws_eks_cluster.main.identity.0.oidc.0.issuer
+#   }
+# }
+
+
+# resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#   role       = aws_iam_role.eks_cluster_role.name
+# }
+
 
 # # # OIDC associate, copy this oidc id after cluster creation and paste
 
@@ -35,3 +42,47 @@ resource "aws_eks_identity_provider_config" "oidc" {
 #   client_id_list  = ["sts.amazonaws.com"]
 #   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd60e22"] # Default EKS thumbprint
 # }
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+
+data "aws_eks_cluster" "main" {
+  name = var.cluster_name
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.aws_eks_cluster.main.identity[0].oidc[0].issuer]
+  url             = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
