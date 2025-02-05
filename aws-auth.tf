f38e406bbc1e8f9465295de3a0afad2660d12b6a
pashaimran <pashaimran68413@gmail.com>
2025-02-05 21:01:01 +0530

    version

--- aws-auth.tf
+++ aws-auth.tf
@@ -1,84 +1,41 @@
-# resource "kubernetes_config_map_v1_data" "aws_auth" {
-#   metadata {
-#     name      = "aws-auth"
-#     namespace = "kube-system"
-#   }
-
-#   data = {
-#     # Adding the admin IAM user to system:masters group
-#     mapUsers = yamlencode([
-#       {
-#         userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.admin_user_name}"
-#         username = var.admin_user_name
-#         groups   = ["system:masters"]
-#       }
-#     ])
-
-#     # Adding the EKS admin role to system:masters group
-#     mapRoles = yamlencode([
-#       {
-#         rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.eks_admin_role}"
-#         username = "admin"  # Use admin username for the EKS admin role
-#         groups   = ["system:masters", "system:bootstrappers"]
-#       },
-
-#       # Adding worker nodes to system:nodes group
-#       {
-#         rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.eks_node_group_role}"  # IAM role for the worker node group
-#         username = "system:node:{{EC2PrivateDNSName}}"  # Dynamically generated username for each node
-#         groups   = ["system:bootstrappers", "system:nodes"]  # Nodes are typically part of bootstrappers and nodes groups
-#       }
-#     ])
-#   }
-
-#   force = true
-#   depends_on = [aws_eks_cluster.main]
-# }
-
-
-# AWS auth ConfigMap
-resource "kubernetes_config_map_v1" "aws_auth" {
+resource "kubernetes_config_map_v1_data" "aws_auth" {
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
   }
 
   data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.eks_node_group_role}"  # Reference to your node group IAM role
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
-      },
-      {
-        rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.eks_admin_role}"  # Reference to your admin IAM role
-        username = "admin"
-        groups   = [
-          "system:masters"
-        ]
-      }
-    ])
+    # Adding the admin IAM user to system:masters group
     mapUsers = yamlencode([
       {
-        userarn  = "arn:aws:iam::264278751395:user/imran"
-        username = "imran"
+        userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.admin_user_name}"
+        username = var.admin_user_name
         groups   = [
           "system:masters",
           "system:nodes"
         ]
       }
     ])
-    force = true
-    depends_on = [aws_eks_cluster.main]
+
+    # Adding the EKS admin role to system:masters group
+    mapRoles = yamlencode([
+      {
+        rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.eks_admin_role}"
+        username = "admin"  # Use admin username for the EKS admin role
+        groups   = ["system:masters", "system:bootstrappers"]
+      },
+
+      # Adding worker nodes to system:nodes group
+      {
+        rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.eks_node_group_role}"  # IAM role for the worker node group
+        username = "system:node:{{EC2PrivateDNSName}}"  # Dynamically generated username for each node
+        groups   = ["system:bootstrappers", "system:nodes"]  # Nodes are typically part of bootstrappers and nodes groups
+      }
+    ])
   }
 
-  # Prevent terraform from overwriting aws-auth when external changes are made
-  lifecycle {
-    ignore_changes = [
-      data
-    ]
-  }
+  force = true
+  depends_on = [aws_eks_cluster.main]
 }
+
+
