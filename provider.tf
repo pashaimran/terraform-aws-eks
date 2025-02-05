6686bba30fe62820b26826bde75eb9b3d05b3ef6
pashaimran <pashaimran68413@gmail.com>
2025-02-05 18:09:16 +0530

    version

--- provider.tf
+++ provider.tf
@@ -38,9 +38,9 @@ provider "helm" {
   }
 }
 
-data "aws_eks_cluster" "main" {
-  name = aws_eks_cluster.main.name
-}
+# data "aws_eks_cluster" "main" {
+#   name = aws_eks_cluster.main.name
+# }
 
 data "aws_eks_cluster_auth" "main" {
   name = aws_eks_cluster.main.name
