057b84e1f3086f0531a19bb8aac8a1606cf0b312
pashaimran <pashaimran68413@gmail.com>
2025-02-05 11:32:04 +0530

    version

--- aws-auth.tf
+++ aws-auth.tf
@@ -1,24 +1,24 @@
-# # Retrieve AWS Account ID Dynamically
-# data "aws_caller_identity" "current" {}
+# # # Retrieve AWS Account ID Dynamically
+# # data "aws_caller_identity" "current" {}
 
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+# resource "kubernetes_config_map" "aws_auth" {
+#   metadata {
+#     name      = "aws-auth"
+#     namespace = "kube-system"
+#   }
 
-  data = {
-    mapRoles = <<YAML
-- rolearn: ${aws_iam_role.eks_node_group_role.arn}
-  username: system:node:{{EC2PrivateDNSName}}
-  groups:
-    - system:bootstrappers
-    - system:nodes
-- rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AdminRole
-  username: admin
-  groups:
-    - system:masters
-YAML
-  }
-}
+#   data = {
+#     mapRoles = <<YAML
+# - rolearn: ${aws_iam_role.eks_node_group_role.arn}
+#   username: system:node:{{EC2PrivateDNSName}}
+#   groups:
+#     - system:bootstrappers
+#     - system:nodes
+# - rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AdminRole
+#   username: admin
+#   groups:
+#     - system:masters
+# YAML
+#   }
+# }
 
