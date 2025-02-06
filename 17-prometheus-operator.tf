227f7c1eb89e43bf123b5de59abfa990766bbeb1
pashaimran <pashaimran68413@gmail.com>
2025-02-06 22:50:49 +0530

    version

--- 17-prometheus-operator.tf
+++ 17-prometheus-operator.tf
@@ -1,25 +1,25 @@
-# resource "helm_release" "prometheus_operator" {
-#   name             = "prometheus-operator-crds"
-#   repository       = "https://prometheus-community.github.io/helm-charts"
-#   chart            = "kube-prometheus-stack"
-#   namespace        = "monitoring"
-#   create_namespace = true
-#   version          = "56.6.2"  # Use the latest stable version
+resource "helm_release" "prometheus_operator" {
+  name             = "prometheus-operator-crds"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+  version          = "56.6.2"  # Use the latest stable version
 
-#   values = [
-#     yamlencode({
-#       prometheusOperator = {
-#         enabled = true
-#       }
-#       alertmanager = {
-#         enabled = true
-#       }
-#       grafana = {
-#         enabled = true
-#       }
-#       prometheus = {
-#         enabled = true
-#       }
-#     })
-#   ]
-# }
+  values = [
+    yamlencode({
+      prometheusOperator = {
+        enabled = true
+      }
+      alertmanager = {
+        enabled = true
+      }
+      grafana = {
+        enabled = true
+      }
+      prometheus = {
+        enabled = true
+      }
+    })
+  ]
+}
