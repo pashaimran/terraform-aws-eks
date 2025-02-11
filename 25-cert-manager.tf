c24ee3336a3b137a7d96e2c080f11172f0ac360f
pashaimran <pashaimran68413@gmail.com>
2025-02-11 15:35:26 +0530

    version

--- 25-cert-manager.tf
+++ 25-cert-manager.tf
@@ -32,4 +32,95 @@ resource "helm_release" "cert_manager" {
     name  = "prometheus.enabled"
     value = "true"
   }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"  # We created it manually above
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.cert_manager.metadata[0].name
+  }
+
+  depends_on = [
+    kubernetes_service_account.cert_manager,
+    kubernetes_cluster_role.cert_manager,
+    kubernetes_cluster_role_binding.cert_manager
+  ]
+}
+
+
+# Create ServiceAccount
+resource "kubernetes_service_account" "cert_manager" {
+  metadata {
+    name      = "cert-manager"
+    namespace = kubernetes_namespace.cert_manager.metadata[0].name
+    labels = {
+      "app.kubernetes.io/name"      = "cert-manager"
+      "app.kubernetes.io/component" = "controller"
+    }
+  }
+
+}
+
+# Create ClusterRole
+resource "kubernetes_cluster_role" "cert_manager" {
+  metadata {
+    name = "cert-manager-controller"
+  }
+
+  rule {
+    api_groups = ["cert-manager.io"]
+    resources  = ["certificates", "certificaterequests", "issuers", "clusterissuers"]
+    verbs      = ["create", "delete", "deletecollection", "patch", "update", "get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["secrets", "events", "configmaps", "services"]
+    verbs      = ["create", "delete", "get", "list", "patch", "update", "watch"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods", "services"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["endpoints"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = ["route.openshift.io"]
+    resources  = ["routes/custom-host"]
+    verbs      = ["create"]
+  }
+}
+
+# Create ClusterRoleBinding
+resource "kubernetes_cluster_role_binding" "cert_manager" {
+  metadata {
+    name = "cert-manager-controller"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.cert_manager.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.cert_manager.metadata[0].name
+    namespace = kubernetes_namespace.cert_manager.metadata[0].name
+  }
 }
