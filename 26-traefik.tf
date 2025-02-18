a521a1ba5bb17bb04f4155a2dec9411346fc3330
pashaimran <pashaimran68413@gmail.com>
2025-02-18 11:42:53 +0530

    version

--- 26-traefik.tf
+++ 26-traefik.tf
@@ -11,10 +11,10 @@ resource "helm_release" "traefik" {
   namespace        = "traefik"
   create_namespace = true
   version          = "23.0.1"
-  set {
-    name  = "dashboard.enabled"
-    value = "true"
-  }
+  # set {
+  #   name  = "dashboard.enabled"
+  #   value = "true"
+  # }
   values = [
     <<EOF
 deployment:
