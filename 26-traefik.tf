3095edc6bd40ba36f82412632c05727b1899d09e
pashaimran <pashaimran68413@gmail.com>
2025-02-18 14:48:59 +0530

    version

--- 26-traefik.tf
+++ 26-traefik.tf
@@ -1,43 +1,43 @@
-resource "kubernetes_namespace" "traefik" {
-  metadata {
-    name = "traefik"
-  }
-}
+# resource "kubernetes_namespace" "traefik" {
+#   metadata {
+#     name = "traefik"
+#   }
+# }
 
-resource "helm_release" "traefik" {
-  name       = "traefik"
-  repository = "https://traefik.github.io/charts"
-  chart      = "traefik"
-  namespace  = kubernetes_namespace.traefik.metadata[0].name
-  create_namespace = true  # Let Helm manage namespace creation
+# resource "helm_release" "traefik" {
+#   name       = "traefik"
+#   repository = "https://traefik.github.io/charts"
+#   chart      = "traefik"
+#   namespace  = kubernetes_namespace.traefik.metadata[0].name
+#   create_namespace = true  # Let Helm manage namespace creation
 
-  values = [
-    <<EOF
-deployment:
-  enabled: true
+#   values = [
+#     <<EOF
+# deployment:
+#   enabled: true
 
-service:
-  enabled: true
-  type: LoadBalancer
-  externalTrafficPolicy: Local
-  annotations:
-    service.beta.kubernetes.io/aws-load-balancer-scheme: "internet-facing"
-    service.beta.kubernetes.io/aws-load-balancer-type: "nlb"
-    service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: "true"
+# service:
+#   enabled: true
+#   type: LoadBalancer
+#   externalTrafficPolicy: Local
+#   annotations:
+#     service.beta.kubernetes.io/aws-load-balancer-scheme: "internet-facing"
+#     service.beta.kubernetes.io/aws-load-balancer-type: "nlb"
+#     service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: "true"
 
-rbac:
-  enabled: true
+# rbac:
+#   enabled: true
 
-ingressRoute:
-  dashboard:
-    enabled: true
-    matchRule: PathPrefix(`/dashboard`)
+# ingressRoute:
+#   dashboard:
+#     enabled: true
+#     matchRule: PathPrefix(`/dashboard`)
 
-ingressClass:
-  enabled: true
-  isDefaultClass: true
+# ingressClass:
+#   enabled: true
+#   isDefaultClass: true
 
 
-EOF
-  ]
-}
+# EOF
+#   ]
+# }
