37be491084ae059e101ac81594d4a9b18707cc19
pashaimran <pashaimran68413@gmail.com>
2025-02-18 16:55:00 +0530

    version

--- 15-alb-ingress-controller.tf
+++ 15-alb-ingress-controller.tf
@@ -1,83 +1,10 @@
-# # # terraform/module/eks/main.tf --- ALB CONTROLLER
-# resource "helm_release" "aws_load_balancer_controller" {
-#   name       = "aws-load-balancer-controller"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   namespace  = "kube-system"
-#   version    = var.alb_controller_version
-
-#   timeout = 6000  # Timeout in seconds, adjust as needed
-
-#   set {
-#     name  = "clusterName"
-#     value = var.cluster_name
-#   }
-
-#   set {
-#     name  = "image.tag"
-#     value = "v2.4.2"
-#   }
-
-# #   set {
-# #     name  = "serviceAccount.create"
-# #     value = "false"
-# #   }
-
-#   set {
-#     name  = "serviceAccount.name"
-#     value = aws_iam_role.alb_role.name
-#   }
-
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = aws_iam_role.alb_role.arn
-# }
-# }
-
-
-# # # Deploy Nginx Ingress after ALB Controller
-# # resource "helm_release" "nginx_ingress" {
-# #   name        = "nginx-ingress"
-# #   repository  = "https://kubernetes.github.io/ingress-nginx"
-# #   chart       = "ingress-nginx"
-# #   namespace   = "ingress-nginx"
-# #   version     = var.ingress_version
-
-# #   set {
-# #     name  = "controller.service.type"
-# #     value = "NodePort"
-# #   }
-
-# #   depends_on  = [helm_release.aws_load_balancer_controller]
-# # }
-
-
-# # terraform/module/eks/variable.tf
-# variable "alb_controller_version" {
-#   description = "Version of AWS Load Balancer Controller Helm chart"
-#   type        = string
-# }
-
-# # variable "ingress_version" {
-# #   description = "Version of Nginx Ingress Helm chart"
-# #   type        = string
-# # }
-
-
-# # # terraform/module/eks/output.tf
-# # output "alb_controller_status" {
-# #   description = "Status of the AWS Load Balancer Controller"
-# #   value       = helm_release.aws_load_balancer_controller.status
-# # }
-
-
 resource "helm_release" "alb_controller" {
   name             = "aws-load-balancer-controller"
   repository       = "https://aws.github.io/eks-charts"
   chart            = "aws-load-balancer-controller"
   namespace        = "kube-system"
   create_namespace = true
-  timeout          = 600  # Increased timeout to 5 minutes
+  timeout          = 600
 
   set {
     name  = "clusterName"
@@ -95,12 +22,12 @@ resource "helm_release" "alb_controller" {
   }
 
   set {
-    name  = "ingressClassName"
+    name  = "ingressClass"
     value = "alb"
   }
 
   set {
-    name  = "defaultIngressClass"
+    name  = "enableServiceController"
     value = "true"
   }
 
@@ -114,21 +41,19 @@ resource "helm_release" "alb_controller" {
     value = data.aws_region.current.name
   }
 
-  # If you're setting this in Terraform with helm_release, it would look like:
-set {
-  name  = "server.insecure"
-  value = "true"
-}
-
-set {
-  name  = "configs.params.server\\.insecure"
-  value = "true"
-}
-
-set {
-  name  = "server.ingress.enabled"
-  value = "true"
-}
+  # Remove these settings as they're not needed and might cause issues
+  # set {
+  #   name  = "server.insecure"
+  #   value = "true"
+  # }
+  # set {
+  #   name  = "configs.params.server\\.insecure"
+  #   value = "true"
+  # }
+  # set {
+  #   name  = "server.ingress.enabled"
+  #   value = "true"
+  # }
   
   depends_on = [aws_iam_role_policy_attachment.alb_controller]
 }
